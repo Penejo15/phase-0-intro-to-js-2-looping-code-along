73d1e6ded4c2291174515f5d90c53ef0575e5637
peter enejo <petstorm101@gmail.com>
2022-06-14 13:22:48 +0100

    javascript looping

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+const celebration = "wedding";
+
+function writeCards( names, celebration ) {
+    let thankYourCards = []
+    for ( let i = 0; i < names.length; i++) {
+        thankYourCards.push(`Thank you, ${names[i]}, for the wonderful ${celebration} gift!`)
+    }
+
+    return thankYourCards
+};  
+
+function countDown(p){
+    let count = p
+    while (count >= 0) {
+        console.log(count);
+        count -= 1
+    }
+}
